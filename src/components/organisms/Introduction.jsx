151bebd3534e8308d4112710dc6cf1c37ebd28f4
david guzman <reyguz421@gmail.com>
2024-10-22 15:40:52 -0600

    select

--- src/components/organisms/Introduction.jsx
+++ src/components/organisms/Introduction.jsx
@@ -1,10 +1,12 @@
 import Header from "../molecules/header";
 import Introduction from "../molecules/Introduction";
+import HacerDenuncia from "../molecules/HacerDenuncia";
 function Introductions() {
     return ( 
         <>
         <Header></Header>
         <Introduction></Introduction>
+        <HacerDenuncia></HacerDenuncia>
         </>
     );
 }
