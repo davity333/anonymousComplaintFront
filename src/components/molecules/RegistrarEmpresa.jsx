aaf5a3bd5c99773f69513fa1e451dc2a16eca59c
david guzman <reyguz421@gmail.com>
2024-10-23 01:03:53 -0600

    añadir empresa

--- src/components/molecules/RegistrarEmpresa.jsx
+++ src/components/molecules/RegistrarEmpresa.jsx
@@ -1,15 +1,38 @@
+import LabelForms from "../atoms/labelForms";
 function RegisterEmpresa() {
     return ( 
         <>
-        <div className="p-10">
-            <div className="flex justify-center">
-                <p className="text-[3.4vh]">Registrar empresa</p>
-            </div>
-
-            <div className="flex justify-center text-[2.3vh] font-light">
-                <p>Ingrese los datos de la empresa para registrarlo en el sistema.</p>
-            </div>
+    <div className="flex gap-10">
+        <div className="flex flex-col">
+            <LabelForms labelform={"Nombre de la empresa"}></LabelForms>
+            <input type="text" className="border-[0.3vh] w-[44vh]" />
         </div>
+
+        <div className="flex flex-col">
+            <LabelForms labelform={"Codigo postal"}></LabelForms>
+            <input type="text" className="border-[0.3vh] w-[44vh]" />
+        </div>
+    </div>
+
+    <div className="flex gap-10 mt-5">
+        <div className="flex flex-col">
+            <LabelForms labelform={"Nombre del dueño de la empresa"}></LabelForms>
+            <input type="text" className="border-[0.3vh] w-[44vh]" />
+        </div>
+
+        <div className="flex flex-col">
+            <LabelForms labelform={"Horario de servicio"}></LabelForms>
+            <input type="text" className="border-[0.3vh] w-[44vh]" />
+        </div>
+    </div>
+
+    <div className="flex flex-col mt-5">
+        <LabelForms labelform={"Ubicación de la empresa"}></LabelForms>
+        <input type="text" className="border-[0.3vh] w-[44vh]" />
+    </div>
+
+    <button className="bg-slate-800 text-white w-[50vh] mt-5">Agregar empresa</button>
+
         </>
     );
 }
--- src/pages/Empresa.jsx
+++ src/pages/Empresa.jsx
@@ -1,11 +1,23 @@
 import Header from "../components/molecules/header";
-import RegisterCompany from "../components/organisms/RegisterCompany";
+import RegistrarEmpresa from "../components/molecules/RegistrarEmpresa";
 
 function Empresa() {
     return ( 
         <>
         <Header></Header>
-        <RegisterCompany></RegisterCompany>
+        <div className="p-10">
+            <div className="flex justify-center">
+                <p className="text-[3.4vh]">Registrar empresa</p>
+            </div>
+
+            <div className="flex justify-center text-[2.3vh] font-light">
+                <p>Ingrese los datos de la empresa para registrarlo en el sistema.</p>
+            </div>
+        </div>
+        
+        <div className="bg-slate-300">
+        <RegistrarEmpresa ></RegistrarEmpresa>
+        </div>
         </>
     );
 }
